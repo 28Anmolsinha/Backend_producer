ef29741b9b4b7dff6c46387b452d92e0a436e23b
28Anmolsinha <mailmeatanmol@gmail.com>
2025-03-28 17:16:32 +0530

    Refractor service

--- src/main/java/com/backend/demo/repository/ComplaintRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.backend.demo.repository;
-//
-//import com.backend.demo.entity.Complaint;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface ComplaintRepository extends JpaRepository<Complaint, Long> {
-//}
